09263327ec505aab7a41da3144d47f2fde10b905
Israel Tavares <israelbulcaotavares@gmail.com>
2023-12-04 14:41:08 -0300

    funções

--- src/app/componente/trecho/page.tsx
+++ src/app/componente/trecho/page.tsx
@@ -5,7 +5,15 @@ export default function Page() {
         <div key="2">Trecho #2</div>,  /* indice 1 */
         <div key="3">Trecho #3</div>,  /* indice 2 */
     ]    
- 
-   
-    return trechos[1]
+    
+    const carros = [
+        <div key='1'>Kawasaki</div>,
+        <div key='2'>Lamborghini</div>,
+        <div key='3'>Ferrari</div>,
+        <div key='4'>Porshe</div>,
+        <div key='5'>Fiat</div>,
+        <div key='6'>Volkswagen</div>,
+    ]
+    console.log(carros)
+    return carros[1]
 }
