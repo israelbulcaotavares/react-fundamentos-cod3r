d516528d24e24fa05e229f8a805324ad65be9b6a
Israel Tavares <israelbulcaotavares@gmail.com>
2023-12-04 11:53:51 -0300

    javascript dentro do jsx

---
+++ src/app/fundamentos/soma/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+  const nome = "João";
+  const idade = 40;
+  return <div>
+    <div>{nome}, {idade}</div>
+    <div>{2 + 7}</div>
+    <div>{2 + 7}</div>
+    <div>{idade * 3}</div>
+    <div>{Math.random()}</div>
+  
+  </div>;
+}
