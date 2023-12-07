695b8fe583711c9ff3b77b8cf8473eeab1aaeaf1
Israel Tavares <israelbulcaotavares@gmail.com>
2023-12-07 16:11:13 -0300

    implemento pagina inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
+import Pagina from "@/components/Pagina";
+import { IconBrandReact } from "@tabler/icons-react";
 
 export default function Page() {
   return (
-      <div>
-        Estou dentro de ABC - DEF - GHI
+    <Pagina titulo="Fundamentos React" subtitulo="Bem vindo ao mundo do react">
+      <div
+        className={`
+        flex flex-col justify-center items-center
+        w-full h-full text-zinc-500
+      `}
+      >
+        <IconBrandReact size={200} stroke={1} />
+        <span className="font-black">Curso fundamentos de React</span>
+        <span className="text-sm">Aqui você vai aprender fundamentos sólidos</span>
       </div>
-  )
+    </Pagina>
+  );
 }
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,9 +1,19 @@
-import { Icon24Hours, Icon360View } from "@tabler/icons-react";
+import { Icon24Hours, Icon360View, IconHome } from "@tabler/icons-react";
 import MenuItem from "./MenuItem";
 
 export default function Menu() {
   return (
     <div className="flex flex-col justify-start items-center w-52 bg-black p-2 gap-2">
+      <MenuItem
+        icone={<IconHome />}
+        texto="Básico"
+        url="/componente/basico"
+      />
+      <MenuItem
+        icone={<IconHome />}
+        texto="Titulo"
+        url="/componente/titulo"
+      />
       <MenuItem
         icone={<Icon24Hours />}
         texto="Pagina #1"
