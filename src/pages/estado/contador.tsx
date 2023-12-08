de3cf6ec17a7edb934a0f3d658e1a33014c8924f
Israel Tavares <israelbulcaotavares@gmail.com>
2023-12-07 22:32:20 -0300

    contador evoluído

--- src/pages/estado/contador.tsx
+++ src/pages/estado/contador.tsx
@@ -4,15 +4,29 @@ import { useState } from "react";
 import { IconMinus, IconPlus } from "@tabler/icons-react";
 
 export default function PaginaContador() {
-  const [num, setNum] = useState(0);
+  const [num, setNum] = useState(0) 
+  const [delta, setDelta] = useState(1)
 
+
+  // function increment() {
+  //   setNum(num + 1);
+  //   console.log(num);
+  // }
+  // function decrement() {
+  //   setNum(num - 1);
+  //   console.log(num);
+  // }
+  function incrementDelta() {
+    setDelta(delta + 1); 
+  }
+  function decrementDelta() {
+    setDelta(delta - 1); 
+  }
   function increment() {
-    setNum(num + 1);
-    console.log(num);
+    setNum(num + delta); 
   }
   function decrement() {
-    setNum(num - 1);
-    console.log(num);
+    setNum(num - delta); 
   }
 
   return (
@@ -25,6 +39,15 @@ export default function PaginaContador() {
           </button>
           <button className="bg-blue-500 rounded-full p-4 hover:bg-blue-400" onClick={increment}>
             <IconPlus size={30}  />
+          </button>
+        </div>
+        <div className="flex gap-5 ">
+        <button className="bg-purple-400 rounded-full p-2 hover:bg-red-400" onClick={decrementDelta}>
+            <IconMinus size={16} />
+          </button>
+          <span>{delta}</span>
+          <button className="bg-purple-500 rounded-full p-2 hover:bg-blue-400" onClick={incrementDelta}>
+            <IconPlus size={16}  />
           </button>
         </div>
       </div>
